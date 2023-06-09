e50b8918eababdb5cbda0d492843be3bba011749
NikkyTheFox <adi.pochec@gmail.com>
2023-06-09 17:41:10 +0200

    better flight control mechanism

--- src/index.js
+++ src/index.js
@@ -101,18 +101,22 @@ modelSelect.addEventListener("change", function () {
         case "aircraft":
             position = entity.position.getValue(viewer.clock.currentTime, new Cartesian3())
             entity = createModel("http://localhost:3000/images/Cesium_Air.glb", position, 1);
+            entity.name = selectedValue;
             break;
         case "passenger_plane":
             position = entity.position.getValue(viewer.clock.currentTime, new Cartesian3())
             entity = createModel("http://localhost:3000/images/Airplane.glb", position, 1);
+            entity.name = selectedValue;
             break;
         case "ww1":
             position = entity.position.getValue(viewer.clock.currentTime, new Cartesian3())
             entity = createModel("http://localhost:3000/images/ww1.glb", position, 1);
+            entity.name = selectedValue;
             break;
         case "falcon":
             position = entity.position.getValue(viewer.clock.currentTime, new Cartesian3())
             entity = createModel("http://localhost:3000/images/falcon.glb", position, 0.1);
+            entity.name = selectedValue;
             break;
     }
 });
@@ -120,25 +124,62 @@ modelSelect.addEventListener("change", function () {
 position = entity.position;
 
 let speed = 0.1; // Initial speed value
-let rotationSpeed = 0.2; // Rotation speed value
+let headingRotationSpeed = 0.02/60; // Rotation speed value\
+let pitchRotationSpeed = 0.2; // Rotation speed value
+let rollRotationSpeed = 0.2; // Rotation speed value
+let acceleration = 0.01; // Speed increment value
 let headingOffset = 0;
 let pitchOffset = 0;
 let rollOffset = 0;
+let minimalSpeed = 0.1
+
+switch(entity.name){//change flight control params based on chosen model
+    case "falcon":
+        speed = 0.69; // Initial speed value
+        headingRotationSpeed = 0.2/60; // Rotation speed value\
+        pitchRotationSpeed = 0.5; // Rotation speed value
+        rollRotationSpeed = 0.5; // Rotation speed value
+        acceleration = 0.069; // Speed increment value
+        break;
+    case "ww1":
+        speed = 0.35; // Initial speed value
+        headingRotationSpeed = 0.3/60; // Rotation speed value\
+        pitchRotationSpeed = 0.5; // Rotation speed value
+        rollRotationSpeed = 0.5; // Rotation speed value
+        acceleration = 0.04; // Speed increment value
+        break;
+    case "passenger_plane":
+        speed = 0.1; // Initial speed value
+        headingRotationSpeed = 0.02/60; // Rotation speed value\
+        pitchRotationSpeed = 0.1; // Rotation speed value
+        rollRotationSpeed = 0.1; // Rotation speed value
+        acceleration = 0.01; // Speed increment value
+        break;
+    case "aircraft":
+        speed = 0.25; // Initial speed value
+        headingRotationSpeed = 0.02/60; // Rotation speed value\
+        pitchRotationSpeed = 0.2; // Rotation speed value
+        rollRotationSpeed = 0.2; // Rotation speed value
+        acceleration = 0.05; // Speed increment value
+        break;
+    default:
+        speed = 0.25; // Initial speed value
+        headingRotationSpeed = 0.02/60; // Rotation speed value\
+        pitchRotationSpeed = 0.2; // Rotation speed value
+        rollRotationSpeed = 0.2; // Rotation speed value
+        acceleration = 0.05; // Speed increment value
+        break;
+}
 
 viewer.clock.onTick.addEventListener(function (clock) {
     const currentPosition = position.getValue(clock.currentTime, new Cartesian3());
     const orientation = entity.orientation.getValue(viewer.clock.currentTime, new HeadingPitchRoll());
-    console.log(rollOffset);
-    /*if(rollOffset > 0){
-        console.log("here");
-        rollOffset =0;
-        console.log(rollOffset);
+    if(rollOffset < 0){            
+        headingOffset -= headingRotationSpeed * (-rollOffset);
     }
-    if(rollOffset < 0){
-        console.log("here2");
-        rollOffset += rotationSpeed
-        console.log(rollOffset);
-    }*/
+    if(rollOffset > 0){
+        headingOffset += headingRotationSpeed * (rollOffset);
+    }
     var hpr = new HeadingPitchRoll(headingOffset, pitchOffset, rollOffset);
     entity.orientation = Transforms.headingPitchRollQuaternion(currentPosition, hpr);
     orientation.roll = rollOffset;
@@ -158,15 +199,13 @@ viewer.clock.onTick.addEventListener(function (clock) {
 });
 
 document.addEventListener('keydown', function (event) {
-    const acceleration = 0.01; // Speed increment value
-
     switch (event.keyCode) {
         case 38: // up arrow
             var currentPosition = entity.position.getValue(viewer.clock.currentTime, new Cartesian3());
             var heading = headingOffset;
             var pitch = pitchOffset;
             var roll = rollOffset;
-            pitch -= Math.toRadians(rotationSpeed);
+            pitch -= Math.toRadians(pitchRotationSpeed);
             pitchOffset = pitch;
             var hpr = new HeadingPitchRoll(heading, pitch, roll);
             entity.orientation = Transforms.headingPitchRollQuaternion(currentPosition, hpr);
@@ -176,7 +215,7 @@ document.addEventListener('keydown', function (event) {
             var heading = headingOffset;
             var pitch = pitchOffset;
             var roll = rollOffset;
-            pitch += Math.toRadians(rotationSpeed);
+            pitch += Math.toRadians(pitchRotationSpeed);
             pitchOffset = pitch;
             var hpr = new HeadingPitchRoll(heading, pitch, roll);
             entity.orientation = Transforms.headingPitchRollQuaternion(currentPosition, hpr);
@@ -186,9 +225,7 @@ document.addEventListener('keydown', function (event) {
             var heading = headingOffset;
             var pitch = pitchOffset;
             var roll = rollOffset;
-            heading -= Math.toRadians(rotationSpeed);
-            headingOffset = heading;
-            //rollOffset -= Math.toRadians(rotationSpeed); need to be fixed
+            rollOffset -= Math.toRadians(rollRotationSpeed);
             roll = rollOffset;
             var hpr = new HeadingPitchRoll(heading, pitch, roll);
             entity.orientation = Transforms.headingPitchRollQuaternion(currentPosition, hpr);
@@ -198,12 +235,18 @@ document.addEventListener('keydown', function (event) {
             var heading = headingOffset;
             var pitch = pitchOffset;
             var roll = rollOffset;
-            heading += Math.toRadians(rotationSpeed);
-            headingOffset = heading;
-            //rollOffset += Math.toRadians(rotationSpeed); need to be fixed
+            rollOffset += Math.toRadians(rollRotationSpeed);
             roll = rollOffset;
             var hpr = new HeadingPitchRoll(heading, pitch, roll);
             entity.orientation = Transforms.headingPitchRollQuaternion(currentPosition, hpr);
-            break;   
+            break;
+        case 32: // space bar
+            speed += acceleration;
+            break;
+        case 17: // ctrl
+            speed -= acceleration;
+            // Limit the speed to a minimum of 0.01
+            speed = (speed > minimalSpeed) ? speed : minimalSpeed;
+            break;
     }
 });
